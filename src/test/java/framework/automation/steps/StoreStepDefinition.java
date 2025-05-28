9e61aea2a169bba18d89cfb94bf6adf2697263ce
Hector Vergara <hector-19-20@hotmail.com>
2025-05-28 11:50:30 -0500

    word accentuations are corrected in the Step

--- src/test/java/framework/automation/steps/StoreStepDefinition.java
+++ src/test/java/framework/automation/steps/StoreStepDefinition.java
@@ -25,26 +25,26 @@ public class StoreStepDefinition {
     }
 
     @Given("The user is on the homepage")
-    public void el_usuario_se_encuentra_en_la_página_principal() {
+    public void elUsuarioSeEncuentraEnLaPaginaPrincipal() {
         DriverManager.getDriver().get("https://opencart.abstracta.us/");
         ScreenshotUtil.captureScreenshot(driver, testContext.getScenario(), "Home Page Loaded");
     }
 
     @When("They enter {string} in the search bar and press search")
-    public void ingresaEnLaBarraDeBúsquedaYPresionaBuscar(String value) {
+    public void ingresaEnLaBarraDeBusquedaYPresionaBuscar(String value) {
         homePage.setInputSearch(value);
         homePage.clickOnSearchButton();
         ScreenshotUtil.captureScreenshot(driver, testContext.getScenario(), "Search Executed for: " + value);
     }
 
     @And("They add the first search result to the cart")
-    public void seleccionaLaPrimeraOpciónQueApareceEnLosResultados() {
+    public void seleccionaLaPrimeraOpcionQueApareceEnLosResultados() {
         homePage.clickOnAddToCartButton();
         ScreenshotUtil.captureScreenshot(driver, testContext.getScenario(), "Added First Search Result to Cart");
     }
 
     @And("The user click on the shopping cart button")
-    public void haceClicEnElBotónDelCarritoDeCompras() {
+    public void haceClicEnElBotonDelCarritoDeCompras() {
         homePage.clickOnItemsCartButton();
         ScreenshotUtil.captureScreenshot(driver, testContext.getScenario(), "Clicked on Cart Button");
     }
